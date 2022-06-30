40f41271d05ac309357e7f31e47b4407669dbda6
Ben Lau <ben.lau@data4life.care>
2022-06-30 14:11:10 +0200

    WIP

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,10 +1,43 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
+import { SUGGESTIONS } from '../../global/constants';
 
 @Component({
   tag: 'app-home',
-  shadow: false,
+  styleUrl: 'app-home.css',
 })
 export class AppRoot {
+  @State() items: Array<string> = [];
+
+  handleTag(event: Event) {
+    console.log(event.target);
+  }
+
+  renderSuggestions() {
+    if (SUGGESTIONS) {
+      return (<div class="suggestions">
+        <h4>Quick suggestions</h4>
+        <ul>{Object.keys(SUGGESTIONS).map((item) => 
+          <li><d4l-tag text={item} classes='suggestions__tag' handleClick={this.handleTag}/></li>
+        )}</ul>
+      </div>);
+    }
+  }
+
+  renderList() {
+    if (this.items.length) {
+      return (
+        <div>
+          <ul class="search-list">
+            {this.items.map((item) => <li>{item}</li>)}
+          </ul>
+          <d4l-button text='Submit' />
+        </div>
+      )
+    } else {
+      return 'Your list is empty. Please enter or select ingredients from the left ←.'
+    }
+  }
+
   render() {
     return (
       <d4l-card classes="card--round-corners-l card--text-center">
@@ -13,8 +46,26 @@ export class AppRoot {
           <p>Let's find a recipe you can cook with.</p>
         </div>
         <div slot="card-content">
-        </div>
+          <div class="search">
+            <div class="search-input">
+              <d4l-input
+                id="search"
+                placeholder='Enter an ingredient'
+                label=''
+              />
+              <d4l-button classes='button--secondary' text='Add to list →' />
+            </div>
 
+            {this.renderSuggestions()}
+
+            <div class="search-params">
+              <h3>Your list</h3>
+              <div class="search-list">
+                {this.renderList()}
+              </div>
+            </div>
+          </div>
+        </div>
       </d4l-card>
     );
   }
